f438c0265e804c38235670bf2d728bee972d6d56
janith <janith.sachintha.dev@gmail.com>
2026-09-18 16:21:15 +0530

    feature done ✅

--- src/pages/client/forgetPassword.jsx
+++ src/pages/client/forgetPassword.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useState } from "react";
-import { useEffect } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
